d2b01075b586fd8f10e133b937e60675142ce801
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-12 12:33:45 -0400

    Had to refactor the code, needed to compare two indices, not two lists

--- exercise5.py
+++ exercise5.py
@@ -5,11 +5,12 @@ Write a function to return True if the first and last number of a given list is 
 different then return False.
 """
 
-# I will begin by writing in a function that takes in two lists and returns a boolean
-# Then, I will compare the two lists by using slicing to check the first and last number are equal
-# To make it short and concise, I will just return the two lists with the equals operator between them
+# I will begin by writing in a function that takes in tone list and returns a boolean
+# Then, I will compare the first and last index to see if they are equal
+# To make it short and concise, I will just return the two substrings with the equals operator between them
 
-def first_and_last1(list1, List[int], list2: List[int]) -> boolean:
-  return lis1t[0] == list2[-1]
+def first_and_last1(lis, List[int]) -> boolean:
+  return lis[0] == lis[-1]
 
-
+print(first_and_last1([10, 20, 30, 40, 10]))
+print(first_and_last1([75, 65, 35, 75, 30]))
