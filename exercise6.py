d6a41d452e2ed18553d5d091aebac52040a9d9ea
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-12 12:47:30 -0400

    Create exercise6.py

---
+++ exercise6.py
@@ -0,0 +1,7 @@
+"""
+Exercise 6: Display numbers divisible by 5 from a list
+
+Iterate the given list of numbers and print only those numbers which are divisible by 5
+"""
+
+# I will create a function that will take in a list of numbers as a parameter that prints out 
