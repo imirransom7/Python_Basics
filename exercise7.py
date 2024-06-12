512805659f7b20d4a6fe3bc63d87eb6247025110
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-12 13:01:36 -0400

    Create exercise7.py, wrote down how I  would solve, started writing the function

---
+++ exercise7.py
@@ -0,0 +1,16 @@
+"""
+Exercise 7: Return the count of a given substring from a string
+
+Write a program to find how many times substring “Emma” appears in the given string.
+"""
+
+# I will create a function that will take in a string and returns a number
+# I will create a variable that will hold counter to count the amount of times 'Emma' shows up
+# To find how many times 'Emma' is in a substring, I will convert the string into a list
+#  with a delimeter of spaces, to then iterate though it and see which elements, substrings, have 
+# Emma as a substring. Next, is to increment the counter everytime Emma is a substring and then return
+# that counter
+
+# creating a function that takes in a a string and returns a number
+def substring(sentence: str) -> int:
+  pass
