977044b981930bc3c6a33a2a4122a400a4b1fc04
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-12 10:23:03 -0400

    wrote down my explanation, will start to solve

--- exercise5.py
+++ exercise5.py
@@ -4,3 +4,10 @@ Exercise 5: Check if the first and last number of a list is the same
 Write a function to return True if the first and last number of a given list is same. If numbers are
 different then return False.
 """
+
+# I will begin by writing in a function that takes in two lists and returns a boolean
+# Then, I will compare the two lists by using slicing to check the first and last number are equal
+# To make it short and concise, I will just return the two lists with the equals operator between them
+
+def first_and_last1(list1, List[int], list2: List[int]) -> boolean:
+  return 
