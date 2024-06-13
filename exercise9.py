7215a38109f4520c718f2b7c379e26f7ba3842d2
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-13 10:19:51 -0400

    Update exercise9.py

--- exercise9.py
+++ exercise9.py
@@ -12,9 +12,12 @@ the palindrome numbers
 # I will use slicing to reverse the number and check to see if it is the same compared with the number without reverese
 
 def palindrome_number(nums: int):
+  # casting str() over the nums parameter to conver to string so that I can reverse it using slicing, which only works with sequences
   if str(nums) == str(nums)[::-1]:
     print("Yes. Given number is palindrome number)
   else:
     print("No. Given number is not palindrome")
 
+
+palindrome_number(121)
 
