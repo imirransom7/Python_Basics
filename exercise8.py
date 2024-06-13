9c3510b4047ab2645ee8e1a39e87f473a6d6e9e1
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-13 07:57:16 -0400

    adding explanation to solve the problem

--- exercise8.py
+++ exercise8.py
@@ -8,3 +8,5 @@ Exercise 8: Print the following pattern:
 5 5 5 5 5
 """
 
+# I will make a function that will take in a list of numbers
+# I will also need a control flow to be able to get the numbers to print out like the example above
