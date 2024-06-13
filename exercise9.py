a1e0f7d794021e515b6b931bb092d3ca41381d77
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-13 10:11:15 -0400

    finished solution

--- exercise9.py
+++ exercise9.py
@@ -6,3 +6,15 @@ Write a program to check if the given number is a palindrome number.
 A palindrome number is a number that is the same after reverse. For example, 545, is
 the palindrome numbers
 """
+
+# Writing a function that will take in a number and print out if the number is a palindrome
+# I will check the number to see if it is a palindrome by casting str() over the number and using slicing
+# I will use slicing to reverse the number and check to see if it is the same compared with the number without reverese
+
+def palindrome_number(nums: int):
+  if str(nums) == str(nums)[::-1]:
+    print("Yes. Given number is palindrome number)
+  else:
+    print("No. Given number is not palindrome")
+
+
