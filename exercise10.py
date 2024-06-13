133f6f594cd53b51046a0154e24f8be6fd732a1e
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-13 15:00:11 -0400

    wrote down my explanation for this problem

--- exercise10.py
+++ exercise10.py
@@ -6,3 +6,10 @@ Create a new list from two list using the following condition
 Given two list of numbers, write a program to create a new list such that the new list
 should contain odd numbers from the first list and even numbers from the second list.
 """
+
+# I will create a function that will take in two list as parameters, and return a list
+# Then, I will iterate through both lists and only retrieve even numbers from one, and odd from the other
+# Then, I will append all of that to a new list and return that list
+
+def even_and_odd1(list1: List[int], list2: List[int]) -> List[int]:
+  pass
