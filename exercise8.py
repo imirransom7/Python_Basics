697181397f260f2887917164935bab519c7ffa11
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-13 08:12:21 -0400

    finished my solution

--- exercise8.py
+++ exercise8.py
@@ -10,3 +10,15 @@ Exercise 8: Print the following pattern:
 
 # I will make a function that will take in a list of numbers
 # I will also need a control flow to be able to get the numbers to print out like the example above
+# When iterating through the list, I will add however many of the element to itself as a string to get
+# it to print out the same number shown
+
+def triangle1(nums: List[int]):
+  # creating a for loop to iterate the numbers
+  for num in nums:
+    # casting str() over the elements/numbers so that I can multiply the number against itself
+    # to have them print a triangle or step
+    print(str(num) * num)
+
+
+traingle1([1, 2, 3, 4, 5])
