a7886a25b01b6e65486b859f87aa69fc798dec64
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-13 15:21:30 -0400

    might have found a solution

--- exercise10.py
+++ exercise10.py
@@ -12,4 +12,8 @@ should contain odd numbers from the first list and even numbers from the second 
 # Then, I will append all of that to a new list and return that list
 
 def even_and_odd1(list1: List[int], list2: List[int]) -> List[int]:
-  pass
+  new_list = [list1[x], list2 for x in range(0, len(list1), 2) for y in range(1, len(list2)), 2]
+    return new_list
+
+
+even_and_odd([10, 20, 25, 30, 35], [40, 45, 60, 75, 90])
