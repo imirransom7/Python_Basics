c5f7432e00969b94e2c6c7ab48219aaf212f12d4
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-17 11:48:15 -0400

    adding exercise 12

---
+++ exercise12.py
@@ -0,0 +1,8 @@
+"""
+Exercise 12: Calculate income tax for the given income by adhering to the riles below
+
+For example, suppose the taxable income is 45000, and the income tax payable is
+
+10000*0% + 10000*10%  + 25000*20% = $6000.
+"""
+
