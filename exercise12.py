0ffcdf48712bf167cceedbdc22930427841504db
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-19 09:08:10 -0400

    Created a solution for this problem

--- exercise12.py
+++ exercise12.py
@@ -7,7 +7,12 @@ For example, suppose the taxable income is 45000, and the income tax payable is
 """
 
 # I am going to create a fuction that will take in one number as the parameter and return on number
-# Then, I will implement the equation used above. I will do (20,000 - num)*20% since I need the remainder
+# Then, I will implement the equation used above. I will do (nums - 20,000)*20% since I need the remainder
 # of the money after the first and second 10,000 used in the equation. Then, I will return the answer
 
+def taxable(nums: int) -> int:
+  # implementing the equation given and subtracting the number given by 20,000 to get the remainder
+    return (10000 * .00) + (10000 * .10) + ((nums - 20,000) * .20)
 
+
+print(taxable(45000))
