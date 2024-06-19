b1e6a57931a344d7ab15feb58dd5a9280978620e
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-19 09:01:50 -0400

    Added my explanation on how I will solve this problem

--- exercise12.py
+++ exercise12.py
@@ -6,3 +6,8 @@ For example, suppose the taxable income is 45000, and the income tax payable is
 10000*0% + 10000*10%  + 25000*20% = $6000.
 """
 
+# I am going to create a fuction that will take in one number as the parameter and return on number
+# Then, I will implement the equation used above. I will do (20,000 - num)*20% since I need the remainder
+# of the money after the first and second 10,000 used in the equation. Then, I will return the answer
+
+
