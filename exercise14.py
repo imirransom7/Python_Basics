1ef3c66b02b5dfbacb3c93f30a83a34283022efb
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-20 09:01:52 -0400

    Created exercise 14; added explanation on how I will solve this problem

---
+++ exercise14.py
@@ -0,0 +1,9 @@
+"""
+Exercise 14: Print a downward Half-Pyramid Pattern of Star
+(asterisk)
+"""
+
+# I will create a function that will take in a number and print out a down Half-Pyramid Patter of Star
+# I will then create a for loop that decrements the number given until it reaches 0.
+# To get the star pattern, I will multiply the current number with a string asterisk: "*" so that
+# I that the same number of stars will print out
