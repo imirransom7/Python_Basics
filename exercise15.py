16875641dc14f9c57434aeeed9c9e436f4e70e29
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-20 16:30:48 -0400

    added my solution to this problem

--- exercise15.py
+++ exercise15.py
@@ -5,3 +5,13 @@ an int value of base raises to the power of exp
 Note here exp is a non-negative integer, and the base is an integer
 """
 
+# I will make a function that will take in two integers as parameters and return a number
+# I will multiply the base with the exponent by using two asterisks (*) and return the result
+
+
+def exponent(base: int, exp: int) -> int:
+    return base**exp
+
+
+print(exponent(2, 5))
+print(exponent(5, 4))
