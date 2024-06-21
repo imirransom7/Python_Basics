cd6765c884333a158ac76e4d9770e7786f071176
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-21 16:53:30 -0400

    finsihed my solution for exercise 5

--- input_and_output_exercises/exercise5.py
+++ input_and_output_exercises/exercise5.py
@@ -12,4 +12,17 @@ Expected Output
 # When the user inputs their numbers, those numbers will be appended to the empty list made in the beginning
 
 
-# Declaring a new list
+# Declaring an empty list
+numbers = []
+
+# creating the range of 5
+for i in range(1, 6):
+    # prompting the user to enter a number and casting float over it so that it will be a float and not a string
+    # using the current index, i, to let the user know what number of inputs they are currently at
+    user_input = float(input(f"Five numbers are needed. You are at number {i}, please type in a number "))
+    # saving the user's inputs into the list created above using the append method, also using the function round()
+    # to only set it a the first decimal place
+    numbers.append(round(user_input, 1))
+
+# printing out the list of numbers inputted by the user
+print(f'\n{numbers}')
