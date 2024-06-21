cd301d220a4a7730f0ea675b9baa880ad9da487e
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-21 08:50:25 -0400

    created exercise 4; wrote down how to solve the problem

---
+++ input_and_output_exercises/exercise4.py
@@ -0,0 +1,15 @@
+"""
+Exercise 4: Display float number with 2 decimal places using print()
+
+Given:
+num = 458.541315
+
+Expected Output
+458.54
+"""
+
+# I am going to make a function that takes in a float as a parameter and return that float
+# with only two decimal places. To do this, I wil b e using the round function
+
+def round_to_two(num: float) -> float:
+    pass
