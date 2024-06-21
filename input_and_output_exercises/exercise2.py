840367389e68705b2a1e28f49883d585664656fb
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-20 23:03:14 -0400

    finished solution for exercise 2

--- input_and_output_exercises/exercise2.py
+++ input_and_output_exercises/exercise2.py
@@ -4,3 +4,8 @@ Exercise 2: Display three string "Name", "Is", "James" as
 "Name**Is*James"
 """
 
+# To get the names to print out like the example above, I will use the print function
+# so that there are three strings and add the sep= method to split them up with
+# delimiter if asterisks ("**")
+
+print("Name", "Is", "James", sep="**")
