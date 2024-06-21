fde721cdad750b58051a53b841221d89ac31a5cc
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-20 22:53:56 -0400

    added my solution to the problem

--- input_and_output_exercises/exercise1.py
+++ input_and_output_exercises/exercise1.py
@@ -4,6 +4,13 @@ Exercise 1: Accept numbers from a user
 Write a program to accept two numbers from the user and calculate multiplication
 """
 
+# saving number inputs to two variables
+number1, number2 = int(input("Type in your first number ")), int(input("Now, type in your second number "))
+
+
 # writing a function that will take in two integers as parameters
-def multiplication(num1: int, num2: int) -> int:
-    pass
+def multiplication(num1: int, num2: int):
+    print(f'\nYour inputted numbers multiplied together is {num1 * num2}')
+
+
+multiplication(number1, number2)
