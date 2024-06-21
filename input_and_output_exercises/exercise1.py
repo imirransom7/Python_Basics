79185fcc2cd897a53cbcd148f6168ca2d088330c
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-20 22:45:13 -0400

    started new exercises, created new exercise for input and outputs

---
+++ input_and_output_exercises/exercise1.py
@@ -0,0 +1,9 @@
+"""
+Exercise 1: Accept numbers from a user
+
+Write a program to accept two numbers from the user and calculate multiplication
+"""
+
+# writing a function that will take in two integers as parameters
+def multiplication(num1: int, num2: int) -> int:
+    pass
