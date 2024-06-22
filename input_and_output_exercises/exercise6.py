2fa8ce15656f91fab913fa4fbf6b7e5e3c45bf21
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-21 21:52:01 -0400

    created the txt file, but it was not created how I wanted it to be; adjusting the code

--- input_and_output_exercises/exercise6.py
+++ input_and_output_exercises/exercise6.py
@@ -17,6 +17,15 @@ line 7
 
 # I will start by creating a new file called test.txt using the open() function
 # then I will write 7 new lines by using a for loop with a range of 7
+# when writing in a new line so that it will be 'line 1', I will use the current index of the loop
+# to put in the number where the line is at
 
+# creating the test.txt file
+with open('test.txt', 'w') as file:
+    # creating the for loop to create the lines for the new file
+    for i in range(1, 8):
+        # every loop, a new line will be written in with the number being the current place of the index
+        file.write(f'line {i}')
 
-
+with open('test.txt', 'r') as file:
+    file.read()
