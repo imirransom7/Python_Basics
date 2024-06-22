7f3d65f4c608fb9ce14a6d6b9f9be5de0d516d9b
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-22 19:42:40 -0400

    created exercise 2

---
+++ string_exercises/exercise2.py
@@ -0,0 +1,16 @@
+"""
+Exercise 2: Append new string in the middle of a given string
+
+Given two strings, s1 and s2. Write a program to create a new string s3 by appending s2 in the
+middle of s1
+"""
+
+# I will start of with making a function that takes in two parameters that are strings
+# Then I will subtract the length of the first string, s1, by two to get the middle so that I can use slicing
+# to fit the s2 in the middle of s1, and then add on the last parts s1 right after s2
+
+# creating my function
+
+
+def put_in_middle(s1: str, s2: str) -> str:
+    pass
