06b57c7cac5940c9efa9a827b0cda470de825e24
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-22 19:07:25 -0400

    created new folder for string exercises, started with creating exercise 1

---
+++ string_exercises/exercise1.py
@@ -0,0 +1,14 @@
+"""
+Exercise 1A: Create a string made of the first, middle, and last
+character
+
+Write a program to create a new string made of an input string's first, middle, and last character
+
+Given:
+
+str1 = "James"
+
+Expected Output:
+Jms
+"""
+
