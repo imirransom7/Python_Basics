b65ea4cfe0609aa408a241a703e1bb3c6f76a4a8
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-21 21:46:01 -0400

    created exercise 6; started explaning my solution

---
+++ input_and_output_exercises/exercise6.py
@@ -0,0 +1,22 @@
+"""
+Exercise 6: Write all content of a given file into a new file by
+skipping line number 5
+
+Create a test,txt file and add the below content to it
+
+Given test.txt file:                                 Expected Output: new_file.txt
+
+line 1                                                line 1
+line 2                                                line 2
+line 3                                                line 3
+line 4                                                line 4
+line 5                                                line 6
+line 6                                                line 7
+line 7
+"""
+
+# I will start by creating a new file called test.txt using the open() function
+# then I will write 7 new lines by using a for loop with a range of 7
+
+
+
