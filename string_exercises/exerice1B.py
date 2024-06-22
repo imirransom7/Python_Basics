3a66ab07eea1c53f62868ae92821c0dcfa0a9eee
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-22 19:33:36 -0400

    finished my solution for this exercise

--- string_exercises/exerice1B.py
+++ string_exercises/exerice1B.py
@@ -15,5 +15,21 @@ Output:                                                         Output:
 Dip                                                             Son
 """
 
+# I will start by making a function with one parameter being a string and returning a string
+# Then, inside the function, I will get the middle letter by using length and dividing it by two.
+# After that is done, I can the next two middle letters by subtracting one to the middle index, and then adding 1
+# to the middle index
 
 
+def middle_three(string: str) -> str:
+    # Getting the middle character by diving the length by two, then adding one more, and
+    # then subtracting one more to get the third middle character
+    # declaring the middle index since I will be using more than once; subtracting by one to get middle letter
+    middle = int(len(string) // 2)
+    # subtracting the first one from the middle to add it to the middle number and then the one right after the middle
+    return string[middle-1] + string[middle] + string[middle+1]
+
+
+# testing out that my function is working
+print(middle_three("JhonDipPeta"))
+print(middle_three("JaSonAy"))
