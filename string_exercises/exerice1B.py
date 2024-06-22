345e3ddded643a4cde7c015b4cd5eefbe3988c72
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-22 19:27:29 -0400

    :created exercise 1B, changed the prevoius exercise to  exercies 1A

---
+++ string_exercises/exerice1B.py
@@ -0,0 +1,19 @@
+"""
+
+Exercise 1B: Create a string made of the middle three characters
+
+Write a program to create a new string made of the middle three characters of an input string
+
+Given:
+
+Case 1                                                          Case 2:
+
+str1 = "JohnDipPeta"                                            str2 = "JaSonAy"
+
+Output:                                                         Output:
+
+Dip                                                             Son
+"""
+
+
+
