4730a7d40691b2293b7fe2fbf8a9daebd8aac380
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-22 19:14:13 -0400

    finished my solution

--- string_exercises/exercise1.py
+++ string_exercises/exercise1.py
@@ -13,9 +13,14 @@ Jms
 """
 
 # Starting with initializing the function that will hold one parameter that is a string
-# After that, I will get the first index of the string, then the middle, and then the last
-# by using slicing. Then I will concatenate them together by adding them together
+# After that, I will get the first index of the string, then the middle, and then the last.
+# Then I will concatenate them together by adding them together
 
 
-def first_middle_last(string: str) -> str:
-    pass
+def first_middle_last(name: str) -> str:
+    # taking the first index and adding to the middle index, then adding the last index
+    return name[0] + name[len(name) // 2] + name[-1]
+
+
+print(first_middle_last("James"))
+print(first_middle_last('Tyler'))
