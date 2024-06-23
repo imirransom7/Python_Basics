8b9d7987828c2c9c64a63b086251241ca52c0586
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-23 12:30:16 -0400

    wrote out how I will solve the problem; began solving it

--- string_exercises/exercise3.py
+++ string_exercises/exercise3.py
@@ -14,3 +14,12 @@ Expected Output
 AJrpan
 """
 
+# Starting with creating my function that will take in two strings as parameters that returns a string
+# Next will be taking s1 and s2's first letter by using [0] and adding them together. Then I will get
+# the middle character by dividing the length of the string by two and flooring it, then add them together.
+# After that, I will get the last letters from both by using [-1] and then add them
+
+
+# creating my function to take in two strings
+def first_mid_last(s1: str, s2: str) -> str:
+    pass
