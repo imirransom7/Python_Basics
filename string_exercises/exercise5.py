6f481c156a47c813550d07b8078bfc2487930772
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-23 17:21:05 -0400

    explained my process to solve this problem

--- string_exercises/exercise5.py
+++ string_exercises/exercise5.py
@@ -12,3 +12,11 @@ Chars = 8
 Digits = 3
 Symbol = 4
 """
+
+# Starting with creating a function that takes in one string as a parameter that prints out three counters
+# I will have three separate counters for the numbers, letters, and symbols. Next, I will then
+# iterate through the string so that I can then count the numbers and such. After that, the function will
+# print out the counters for the letters, numbers, and symbols
+
+def let_num_sym(string: str):
+    pass
