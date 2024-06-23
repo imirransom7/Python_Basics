fb51e1d2a550317d86ccc11e191e429ca6b16874
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-23 15:10:55 -0400

    wrote out how I will solve the problem

--- string_exercises/exercise4.py
+++ string_exercises/exercise4.py
@@ -5,3 +5,10 @@ should come first
 Given string contains a combination of the lower and upper case letters. Write a program ro
 arrange the characters of a string so that lowercase letters should come first
 """
+
+# I will start by making a function that takes in a string as a parameter that returns a string
+# Then I will check for the string's lowercase letters and save it to a variable, and do the same
+# for the uppercase letters to a different variable. After that is done, all that is needed is to concatenate
+# them both together, making sure the lowercase letters are first
+
+
