d43490c9d9c1e8ecfbf52367b6cc64de9654eb7a
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-23 15:28:32 -0400

    added exercise 4

---
+++ string_exercises/exercise5.py
@@ -0,0 +1,14 @@
+"""
+Exercise 5: Count all letters, digits, and special symbols from a given
+string
+
+Given
+str = "P@#yn26at^&i5ve"
+
+Expected Outcome:
+Total counts of chars, digits, and symbols
+
+Chars = 8
+Digits = 3
+Symbol = 4
+"""
