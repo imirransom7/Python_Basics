61afd64bc024b8d982983a9083c585e9c7f8b813
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-23 12:39:30 -0400

    created exercise 4

---
+++ string_exercises/exercise4.py
@@ -0,0 +1,7 @@
+"""
+Exercise 4: Arrange the string characters such that lowercase letters
+should come first
+
+Given string contains a combination of the lower and upper case letters. Write a program ro
+arrange the characters of a string so that lowercase letters should come first
+"""
