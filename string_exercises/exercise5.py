3a15d08779f1c96422deafd95359b818058ff9bb
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-23 20:16:34 -0400

    created a solution, checking if it works

--- string_exercises/exercise5.py
+++ string_exercises/exercise5.py
@@ -18,5 +18,25 @@ Symbol = 4
 # iterate through the string so that I can then count the numbers and such. After that, the function will
 # print out the counters for the letters, numbers, and symbols
 
+
 def let_num_sym(string: str):
-    pass
+    # declaring three separate counters
+    letters, digits, symbols = 0, 0, 0
+    # iterating through the string
+    for s in string:
+        # checking if element is a letter
+        if s.islower():
+            # adding to counter
+            letters += 1
+        # checking for numbers:
+        elif s.isnumeric():
+            # adding to counter
+            digits += 1
+        # if it is not a number or letter, then it is a symbol
+        else:
+            symbols += 1
+
+        print(f"Letters = {letters}", f"\nDigits = {digits}", f"\nSymbols = {symbols}")
+
+
+
