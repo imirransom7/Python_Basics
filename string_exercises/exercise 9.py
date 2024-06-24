df88d9fae776a1be2472e610ddd4bb614ae07c94
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-24 10:40:13 -0400

    wrote out how I will solve this problem

--- string_exercises/exercise 9.py
+++ string_exercises/exercise 9.py
@@ -10,3 +10,11 @@ str1 = "PYnative29@#8496"
 Expected Outcome
 Sum is: 38 Average is 6.333333333333
 """
+
+# staring with making a function that takes in one parameter that is a string, and printing out the sum and
+# average of the numbers present in the string. I will find the numbers by iterating through the string to get the
+# numbers using the isnumeric() method. After I get the numbers, I will then print out their sum and average
+
+
+def numbers_only(s1: str):
+    pass
