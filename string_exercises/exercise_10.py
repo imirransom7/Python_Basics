c8bc594331006c6932735cc1e4aa5c05d2dc9c6a
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-24 14:13:02 -0400

    wrote my steps for solving this problem

--- string_exercises/exercise_10.py
+++ string_exercises/exercise_10.py
@@ -8,3 +8,7 @@ str1: "Apple"
 Expected Outcome:
 {'A': 1, 'p': 2, 'l': 1, 'e': 1}
 """
+
+# starting with writing a function that takes in a string as a parameter that returns a dictionary
+# Then I will iterate through the string and count the amount of times a letter occurs. I will place the
+# letter as the key and their occurrences as the value
