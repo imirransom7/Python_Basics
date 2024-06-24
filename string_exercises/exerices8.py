c9554bb75b25c7f8f57ba22ea2e910a9cac5f8e6
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-24 08:57:05 -0400

    added exercise 8

---
+++ string_exercises/exerices8.py
@@ -0,0 +1,12 @@
+"""
+Exercise 7: Find all occurrences of a substring in a given string by
+ignoring the case
+
+Write a program to find all occurrences of "USA" in a given string ignoring the case
+
+Given:
+str1 = "Welcome to the USA. usa awesome, isn't it?"
+
+Expected Outcome
+The USA count is: 2
+"""
