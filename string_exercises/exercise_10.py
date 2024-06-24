fdd089155461261e4f4c6543970229a18e5066a3
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-24 14:19:54 -0400

    completed my solution; checking to see if it is correct

--- string_exercises/exercise_10.py
+++ string_exercises/exercise_10.py
@@ -12,3 +12,17 @@ Expected Outcome:
 # starting with writing a function that takes in a string as a parameter that returns a dictionary
 # Then I will iterate through the string and count the amount of times a letter occurs. I will place the
 # letter as the key and their occurrences as the value
+
+
+def occurrences(str1: str) -> dict:
+    # creating an empty dictionary
+    new_dict = {}
+    # iterating through the given string
+    for s in str1:
+        # checking to see if the letter has already been put in the dictionary
+        if s in new_dict:
+            # if letter is in the dictionary, then increment the value by one
+            new_dict[s] += 1
+        # if the letter is not in the dictionary, then it will be added with a value starting at one
+        else:
+            new_dict[s] = 1
