d19cbb1d767189d0a25f157e02bca08038abe37a
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-24 14:40:30 -0400

    wrote down how I will solve this problem

--- string_exercises/exercise_11.py
+++ string_exercises/exercise_11.py
@@ -8,3 +8,9 @@ Expected Output:
 evitanYP
 """
 
+# I will create a function that takes in a string as a parameter that returns a string
+# to reverse the string, I will use slicing to have the start, stop, and step go in reverse
+
+
+def reverse(str1: str) -> str:
+    pass
