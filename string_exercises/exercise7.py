0a6d34200305e9bcab080c42979f151171dbea9e
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-23 21:40:29 -0400

    added exercise 7

---
+++ string_exercises/exercise7.py
@@ -0,0 +1,15 @@
+"""
+Exercise 7: String characters balance Test
+
+Write a program to check if two strings are balanced. For example, strings s1 and s2 are balanced if
+all the characters in the s1 and s2. The character's position doesn't matter
+
+Given:
+
+Case 1:                                                     Case 2:
+s1 = "Yn"                                                   s1 = "Ynf"
+s2 = "PYnative"                                             s2 = "PYnative"
+
+Expected Output:                                            Expected Output:
+True                                                        False
+"""
