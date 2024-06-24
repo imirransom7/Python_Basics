2b42d2a0d02f9eebed1ec18ee84151c2d03413ee
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-24 14:56:47 -0400

    added another solution

--- string_exercises/exercise_11.py
+++ string_exercises/exercise_11.py
@@ -14,3 +14,26 @@ evitanYP
 
 def reverse(str1: str) -> str:
     return str1[::-1]
+
+
+print(reverse("PYnative"))
+
+# Another solution to this problem
+
+
+# def reverses(str1: str) -> str:
+#     # creating an empty string
+#     str2 = ""
+#     # iterating through the given string using its length as the range; iterating backwards
+#     for i in range(len(str1) - 1, 0, -1):
+#         # adding each letter the empty string
+#         str2 += str1[i]
+#     # returning the reversed string
+#     return str2
+
+def reversing(str1: str) -> str:
+    # using the join() method with the reverse function to reverse the given string
+    return "".join(reversed(str1))
+
+
+print(reversing("PYnative"))
