bd3c9809f7f6ccb7d9d7c5705cbf8c902ab458ea
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-25 13:28:52 -0400

    updated how I solved the problem

--- string_exercises/exercise_12.py
+++ string_exercises/exercise_12.py
@@ -12,7 +12,7 @@ Last occurrence of Emma starts at index 43
 
 # I will create a function that will take two strings as parameters that returns an integer
 # To find the last occurrence of Emma where that index is, I can use the builtin method .rfind(), which
-# returns the last occurrence of the inputted element's index. Once that is done, I will return the index
+# returns the last occurrence of the inputted strings index. Once that is done, I will return the index
 
 
 def last_index(str1: str, str2: str) -> int:
