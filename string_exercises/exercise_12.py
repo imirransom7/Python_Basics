0e288ccc3a25e7a90f57b27984ca861f0508f0db
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-25 13:22:46 -0400

    wrote down how I will solve this problem

--- string_exercises/exercise_12.py
+++ string_exercises/exercise_12.py
@@ -9,3 +9,12 @@ str1 = "Emma is a data scientist who knows Python. Emma works at Google."
 Expected OutputL
 Last occurrence of Emma starts at index 43
 """
+
+# I will create a function that will take a string as a parameter that returns an integer
+# To find the last occurrence of Emma where that index is, I can use the builtin method .rfind(), which
+# returns the last occurrence of the inputted element's index. Once that is done, I will return the index
+
+
+def last_index(str1: str) -> int:
+    pass
+
