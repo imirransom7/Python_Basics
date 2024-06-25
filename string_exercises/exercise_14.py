d0707c4e6a49fb6a9ee7c2934f9b859e2f34d0c6
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-25 15:26:10 -0400

    added how I will solve the problem

--- string_exercises/exercise_14.py
+++ string_exercises/exercise_14.py
@@ -12,3 +12,12 @@ Original list of sting
 After removing empty strings
 ['Emma', 'Jon', 'Kelly', 'Eric']
 """
+
+# I will start by creating a function that takes in a string and returns a list. I will remove the elements that
+# are None values or are empty strings by using a conditional that checks the length of the string. if the length
+# of the string is 0, then it will be removed
+
+
+def no_empty_str(str1: str) -> list[str]:
+    pass
+
