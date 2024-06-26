4aabdea53b9d5b61ee5d69459107ecfee42a4a40
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-26 10:12:04 -0400

    added how I will solve this problem

--- string_exercises/exercise_15.py
+++ string_exercises/exercise_15.py
@@ -7,3 +7,8 @@ str1 = "/*Jon is @developer & musician"
 Expected Output
 Jon is developer musician"
 """
+
+# To start, I will create a function that will take in a string as a parameter and return a string
+# Then, I will convert the string into a list using the split method splitting by the spaces found in 
+# the string. Then I will use a condition while the list is being iterated through to check if elements
+# are letters or symbols. Then I will use the join() method to convert the list back into a string
