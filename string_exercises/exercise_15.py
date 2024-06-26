324af6a13434dfdd70f9460e791da17378df19d0
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-26 10:12:49 -0400

    beginning to solve the problem

--- string_exercises/exercise_15.py
+++ string_exercises/exercise_15.py
@@ -12,3 +12,7 @@ Jon is developer musician"
 # Then, I will convert the string into a list using the split method splitting by the spaces found in 
 # the string. Then I will use a condition while the list is being iterated through to check if elements
 # are letters or symbols. Then I will use the join() method to convert the list back into a string
+
+
+def no_symbols(str1: str) -> str:
+  pass
