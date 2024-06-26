55dae4969dd5a8e2d6666370dba533c63b0242db
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-26 08:01:11 -0400

    found new solutions

--- string_exercises/exercise_14.py
+++ string_exercises/exercise_14.py
@@ -38,3 +38,24 @@ def no_empty_str(str_list: list) -> list[str]:
 
 
 print("Original list of strings\n", s_list, "\n\nAfter removing empty strings\n", no_empty_str(s_list))
+
+# Found other solutions
+
+res_list = []
+# checks if the element is True, meaning if the element is an empty string or None value it#
+# will not be added to the new list
+for s in str_list:
+    # check for non empty string
+    if s:
+        res_list.append(s)
+print(res_list)
+
+
+# using built-in function filter to filter empty value, passing in None as an arguement will result in
+# filter out any None values and empty strings
+new_str_list = list(filter(None, str_list))
+
+print("After removing empty strings")
+print(new_str_list)
+
+
