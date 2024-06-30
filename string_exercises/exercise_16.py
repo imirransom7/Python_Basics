acba242f83c0146ffdaddb92b4032a5ce90044fd
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-29 20:35:47 -0400

    wrote out how I will solve this problem

--- string_exercises/exercise_16.py
+++ string_exercises/exercise_16.py
@@ -7,3 +7,8 @@ str1 = 'I am 25 years and 10 months old'
 Expected Output:
 2510
 """
+
+# I am going to create a function that will take in a string as a parameter and return a number from all
+# the numbers combined from the string. I will iterate through the string and extract the numbers using the
+# isnumeric() method. Then, I will return the string number while casting int() over it so that it is converted
+# into an integer
