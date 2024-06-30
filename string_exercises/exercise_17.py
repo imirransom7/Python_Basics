f34787ba1467ec0f28e1c2a162edbeb9bc1fd195
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-06-29 20:46:28 -0400

    coded a solution

--- string_exercises/exercise_17.py
+++ string_exercises/exercise_17.py
@@ -10,3 +10,13 @@ Expected Output:
 Emma25
 scientist50
 """
+
+
+# making a function that takes in a string and returns a string
+def string_and_numbers(str1: str) -> str:
+    # iterating through a list that is the string split up by spaces
+    for element in str1.split(" "):
+        # checking if the element string contains both a number and a string
+        if element.isnumeric() and element.isalpha():
+            # printing out the elements that meet the condition
+            print(element)
